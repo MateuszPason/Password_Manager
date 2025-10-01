bbf7f6c2dbb784aca6fcd1b3eaab0b9d710095ed
MateuszPason <mateuszpason64@gmail.com>
2025-10-01 19:43:10 +0200

    Dependency injection implementation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from ui.app_gui import PasswordManagerApp
 from core.database import initialize_database
 from ui.login_gui import LoginWindow
+from controllers.credential_controller import CredentialController
+from controllers.ui_controller import UIController
 
 def main():
     initialize_database()
@@ -11,7 +13,9 @@ def main():
 
     if master_password:
         # Launch the main app window
-        app = PasswordManagerApp(master_password)
+        credential_controller = CredentialController(master_password)
+        ui_controller = UIController(credential_controller)
+        app = PasswordManagerApp(master_password, credential_controller, ui_controller)
         app.run()
 
 if __name__ == "__main__":
